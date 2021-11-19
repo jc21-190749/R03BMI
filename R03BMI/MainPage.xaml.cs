a75070fc4a8098b23eeda714c4e9e6a38ffecce0
志苑 大沼 <190749@jc-21.jp>
2021-11-19 10:59:16 +0900

    BMI作成

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -19,6 +19,48 @@ namespace R03BMI
         private void Button_Clicked(object sender, EventArgs e)
         {
 
+
+           string height = heighit.Placeholder;
+            string weight = weighit.Placeholder;
+             string zyoutai="肥満(4度)";
+            try
+            {
+                double h = double.Parse(height);
+                double w = double.Parse(weight);
+
+                if (h>10)
+                {
+                    h = h / 100;
+                }
+    double BMI = w / (h * h);
+    BMI = Math.Round(BMI* 10) / 10;
+               
+                if(BMI<40.00){
+                    string zyoutai="肥満(3度)";
+                  }else if (BMI < 35.00)
+                {
+                    string zyoutai="肥満(2度)";
+                }else if (BMI < 30.00)
+                {
+                    string zyoutai="肥満(1度)";
+                }else if (BMI < 25.00)
+                {
+                    string zyoutai="普通体重";
+                }else if (BMI < 18.50)
+                {
+                    string zyoutai="低体重";
+                }
+                
+                result.Text ="BMIは"+BMI+"で、体形は"+zyoutai+"す。";
+            }
+           
+            catch (FormatException ex)
+            {
+    Console.WriteLine(ex);
+    result.Text = "!!!";
+            }
         }
-    }
+    } 
+    
 }
+

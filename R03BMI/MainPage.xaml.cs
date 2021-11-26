d75207124e5da37285b98af640e812bdcb3a0d74
志苑 大沼 <190749@jc-21.jp>
2021-11-26 10:46:41 +0900

    BMI更新

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -19,7 +19,7 @@ namespace R03BMI
         private void Button_Clicked(object sender, EventArgs e)
         {
 
-            
+            　
             String height = heighit.text;
             String weight = weighit.text;
             
